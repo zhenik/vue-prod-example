9334b944e9885d46f9edc13a5fbbd0c3a44097bf
zhenik <nikita.zhevnitskiy@gmail.com>
2020-04-14 19:21:59 +0200

    refactor auth creds

--- frontend/src/components/auth/TestAuth.vue
+++ frontend/src/components/auth/TestAuth.vue
@@ -2,14 +2,23 @@
   <div id="checking-auth">
     <div class="user-state">
       <h2>Test auth component</h2>
+      <h3>User state</h3>
+      <p>{{user}}</p>
     </div>
   </div>
 </template>
 
 <script>
+  import {mapGetters} from "vuex";
 
+  export default {
+    computed: {
+      ...mapGetters({
+        user: "user",
+      }),
+    },
+  }
 </script>
-
 <style scoped>
 
 </style>
